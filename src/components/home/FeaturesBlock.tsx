ce7009efe0763193cf76f1ffa133b29e5b865194
raipen <raipen@knu.ac.kr>
2024-06-27 12:58:43 +0900

    feat: feature block

---
+++ src/components/home/FeaturesBlock.tsx
@@ -0,0 +1,45 @@
+import Image, { StaticImageData } from 'next/image';
+import {
+  FeatrueInfoContainer, FeatureContainer, FeatureTitle,ColorableText, FeatureImage,
+  FeatureDescription, FeatureLink, CommentContainer, FeatureComment, BoldableText
+} from '@/styles/Home.styles';
+
+import React from 'react';
+
+export type FeatureType = {
+  title: {text: string, color: boolean}[];
+  description: string;
+  linkText: string;
+  image: StaticImageData;
+  imageAlt: string;
+  comment: {text: string, bold: boolean}[][];
+  direction: 'left' | 'right';
+  background: boolean;
+}
+
+export default function FeaturesBlock({title, description, linkText, image, imageAlt, comment, direction, background}: FeatureType) {
+  return (
+    <FeatureContainer $background={background}>
+      {direction === 'left' && <FeatureImage src={image} alt={imageAlt}/>}
+      
+      <FeatrueInfoContainer>
+        <FeatureTitle>
+          {title.map((text, index) => <ColorableText key={index} $color={text.color}>{text.text}</ColorableText>)}
+        </FeatureTitle>
+        <FeatureDescription>{description}</FeatureDescription>
+        <FeatureLink href="/signup">{linkText}</FeatureLink>
+        <CommentContainer>
+          {comment.map((comment, index) => (
+            <FeatureComment key={index} $position={index % 2 === 1 ? 'left' : 'right'}>
+              {comment.map((text, index) => (
+                <BoldableText key={index} $bold={text.bold}>{text.text}</BoldableText>
+              ))}
+            </FeatureComment>
+          ))}
+        </CommentContainer>
+      </FeatrueInfoContainer>
+
+      {direction === 'right' && <FeatureImage src={image} alt={imageAlt}/>}
+    </FeatureContainer>
+  );
+}
--- src/styles/Home.styles.tsx
+++ src/styles/Home.styles.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { styled } from 'styled-components';
-import { clickable } from '.';
+import { clickable, FlexColumnCenter, FlexColumnLeftStart, FlexRowCenter } from '.';
 import { GtagForClick } from '@/utils/GtagForClick';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const TopTitle = styled.h1<{ $color?: boolean }>`
     user-select: none;
@@ -32,4 +34,106 @@ export const LoginButton = styled.button`
     border: 1px solid var(--main-color);
     border-radius: 5px;
     transition: all 0.3s;
+`;
+
+export const FeatureContainer = styled.div<{ $background?: boolean }>`
+    ${FlexRowCenter};
+    position: relative;
+    gap: 2rem;
+    padding: 5rem 0;
+    &::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        width: 100vw;
+        height: 100%;
+        background: ${({ $background }) => $background ? "#eaeaea" : "white"};
+        z-index: -1;
+    }
+`;
+
+export const FeatureTitle = styled.h2<{ $color?: boolean }>`
+    white-space: pre-wrap;
+    font-size: 2.5rem;
+    font-weight: 600;
+    text-align: left;
+`;
+
+//color면 #d65554에서 #ff7d7e로 그라데이션
+export const ColorableText = styled.span<{ $color?: boolean }>`
+    ${({ $color }) => !$color ? "color: black;" : `color: transparent; background: linear-gradient(90deg, #d65554, #ff7d7e); -webkit-background-clip: text; background-clip: text;`}
+`;
+
+export const BoldableText = styled.span<{ $bold?: boolean }>`
+    ${({ $bold }) => $bold ? "font-weight: 600;" : "font-weight: 400;"}
+`;
+
+export const FeatureImage = styled(Image)`
+    max-width: 50%;
+    height: auto;
+`;
+
+export const FeatrueInfoContainer = styled.div`
+    ${FlexColumnLeftStart};
+    gap: 1rem;
+    max-width: 50%;
+    word-break: keep-all;
+`;
+
+export const FeatureDescription = styled.p`
+    font-size: 1.2rem;
+    font-weight: 400;
+    text-align: left;
+`;
+
+export const FeatureLink = styled(Link)`
+    ${clickable};
+    font-size: 1.2rem;
+    font-weight: 400;
+    color: var(--main-color);
+    transition: all 0.3s;
+    &:hover {
+        color: var(--main-color-dark);
+    }
+    &::after {
+        content: '→';
+        margin-left: 0.5rem;
+        font-weight: 600;
+    }
+`;
+
+export const CommentContainer = styled.div`
+    ${FlexColumnCenter};
+    gap: 1rem;
+    margin-top: 1rem;
+    white-space: pre-wrap;
+    width: 100%;
+`;
+
+export const FeatureComment = styled.div<{ $position: 'left' | 'right' }>`
+    font-size: 1rem;
+    align-self: ${({ $position }) => $position === 'left' ? 'flex-start' : 'flex-end'};
+    text-align: left;
+    max-width: 70%;
+    padding: 1rem;
+    background: rgb(255 236 236);
+    border-radius: 5px;
+    position: relative;
+    &::before {
+        content: '';
+        position: absolute;
+        top: calc(50% - 10px);
+        border: 20px solid transparent;
+        border-bottom: 0;
+        ${({ $position }) => $position === 'left' ? `
+            left: -20px;
+            border-right-color: rgb(255 236 236);
+            border-left: 0;
+        ` : `
+            right:-20px;
+            border-left-color: rgb(255 236 236);
+            border-right: 0;
+        `}
+        
+    }
 `;
