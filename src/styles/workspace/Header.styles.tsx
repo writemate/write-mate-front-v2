e976490254844116b6b6016d40bd8be5b73db4dc
raipen <raipen@knu.ac.kr>
2024-08-21 00:53:08 +0900

    feat: add RightArrow.svg and update Header component

--- src/styles/workspace/Header.styles.tsx
+++ src/styles/workspace/Header.styles.tsx
@@ -1,13 +1,46 @@
 'use client';
 import { styled } from 'styled-components';
-import { clickable } from '..';
+import { clickable, FlexRowCenter, FlexRowLeftStart } from '..';
 import { GtagForClick } from '@/utils/GtagForClick';
 
 export const HeaderContainer = styled.header`
-  padding: 20px max(calc(50% - 600px), 20px);
+  ${FlexRowLeftStart};
   width: 100%;
-  display: flex;
-  align-items: center;
-  user-select: none;
-  height: 80px;
+  height: 72px;
+  background-color: #ffffff;
+  border-bottom: 1px solid #D7DCE7;
 `;
+
+export const HeaderTitle = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  margin: 0 24px 0 28px;
+`;
+
+export const SaveStatus = styled.div`
+  ${FlexRowCenter};
+  font-size: 14px;
+  color: var(--cod-gray);
+`;
+
+export const VersionControlButton = styled.div`
+  ${clickable}
+  ${FlexRowCenter};
+  margin-left: auto;
+  font-size: 16px;
+  color: var(--cod-gray);
+  background-color: #ffffff;
+  border: none;
+  padding: 8px 12px 8px 24px;
+  gap: 8px;
+  border-radius: 100px;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.12);
+`;
+
+export const RightContainer = styled.div`
+  ${FlexRowLeftStart};
+  margin-left: 16px;
+  margin-right: 28px;
+  gap: 12px;
+`;
+
