d0a49dcccb3e36e409f841d5fbd890187cabf720
raipen <raipen@knu.ac.kr>
2024-08-20 21:29:35 +0900

    feat: add logout button to Footer component

--- src/components/workspcae/Footer.tsx
+++ src/components/workspcae/Footer.tsx
@@ -1,11 +1,14 @@
 'use client';
+import { useLogin } from "@/stores/useLogin";
 import {  } from "@/styles/workspace/Footer.styles";
 import Link from "next/link";
 
 export default function Footer() {
+    const { logout } = useLogin();
     
     return (
         <>
+            <button onClick={logout}>로그아웃</button>
         </>
     );
 }
