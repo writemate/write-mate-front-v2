ea36664d81ef50fe0e89dc86fb5d1dc0af103164
raipen <raipen@knu.ac.kr>
2024-09-04 18:02:48 +0900

    feat: apply usePlotSidebar hook to PlotSidebar component

--- src/components/workspace/plot/PlotSidebar.tsx
+++ src/components/workspace/plot/PlotSidebar.tsx
@@ -1,12 +1,21 @@
 'use client';
 import { SidebarContainer } from "@/styles/workspace";
+import usePlotSidebar from "@/hooks/workspace/sidebar/usePlotSidebar";
 import Link from "next/link";
-import { useLogin } from "@/stores/useLogin";
+import { Folder, File } from "@/components/workspace/SideBar";
 
 export default function PlotSidebar() {
+    const { rootFolder, isLoading, error, handleToggleFolder } = usePlotSidebar();
     return (
         <SidebarContainer>
-
+            {isLoading && <div>Loading...</div>}
+            {error && <div>Error</div>}
+            {rootFolder && rootFolder.map((file) => {
+                if (file.isFolder) {
+                    return <Folder key={file.folder_name} folder={file} handleToggleFolder={handleToggleFolder} />;
+                }
+                return <File key={file.file_name} file={file} />;
+            })}
         </SidebarContainer>
     );
 }
