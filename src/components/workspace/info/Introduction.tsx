836a3ad751d6e36af0f88379f812509aa03e3da5
raipen <raipendalk@gmail.com>
2024-09-29 02:05:18 +0900

    feat: Update introduction textarea in workspace info page

--- src/components/workspace/info/Introduction.tsx
+++ src/components/workspace/info/Introduction.tsx
@@ -1,12 +1,16 @@
 'use client';
 import { Container, SubTitle } from "@/styles/workspace/Info.style";
 import { TextArea } from "@/styles";
+import { useContext } from 'react';
+import { InfoContext } from "@/hooks/workspace/info";
 
 export default function ScriptSidebar() {
+    const { data, isLoading, onChangeIntroduction} = useContext(InfoContext);
+    const { introduction } = data ?? {};
     return (
         <Container>
             <SubTitle>작품 소개</SubTitle>
-            <TextArea placeholder={"줄거리 내용을 적어주세요.\n연재처 정보란에 들어갈 줄거리 내용을 적어도 좋아요."} rows={5}/>
+            <TextArea placeholder={"줄거리 내용을 적어주세요.\n연재처 정보란에 들어갈 줄거리 내용을 적어도 좋아요."} rows={5} onChange={onChangeIntroduction} defaultValue={introduction} disabled={isLoading}/>
         </Container>
     );
 }
