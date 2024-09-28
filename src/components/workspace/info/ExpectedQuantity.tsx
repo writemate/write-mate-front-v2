7948f13718a56fd4244a8b575912370660da9076
raipen <raipendalk@gmail.com>
2024-09-29 04:55:19 +0900

    Refactor: Update ExpectedQuantity component in workspace info page

--- src/components/workspace/info/ExpectedQuantity.tsx
+++ src/components/workspace/info/ExpectedQuantity.tsx
@@ -8,13 +8,14 @@ import { InfoContext } from "@/hooks/workspace/info";
 export default function ScriptSidebar() {
   const { data, isLoading, onChangeExpectedQuantity, onChangeGrade } = useContext(InfoContext);
   const { expectedQuantity } = data ?? {};
+  const realExpectedQuantity = (expectedQuantity ?? 0)>0?expectedQuantity:undefined;
   const grade = data?.grade ?? null;
 
   return (
     <Container>
       <SubTitle>예상 분량</SubTitle>
       <TextWithDropMenu style={{ maxWidth: 392 }}>
-        <Input type="number" placeholder="예상 분량을 적어주세요" defaultValue={expectedQuantity} onChange={onChangeExpectedQuantity} disabled={isLoading} />
+        <Input type="number" placeholder="예상 분량을 적어주세요" defaultValue={realExpectedQuantity} onChange={onChangeExpectedQuantity} disabled={isLoading} />
         <DropdownMenu placeholder="이용 등급" options={["전체 이용가", "12세 이용가", "15세 이용가", "19세 이용가"] as const} selected={grade} setSelected={onChangeGrade} />
             </TextWithDropMenu>
     </Container>
