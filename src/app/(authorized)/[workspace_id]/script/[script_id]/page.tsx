0b7971b3e0881856e653c07f3d0298f6abe92b8f
raipen <raipendalk@gmail.com>
2024-10-16 21:39:21 +0900

    fix: script editor margin top

--- src/app/(authorized)/[workspace_id]/script/[script_id]/page.tsx
+++ src/app/(authorized)/[workspace_id]/script/[script_id]/page.tsx
@@ -1,25 +1,20 @@
 "use client";
 import { useRef } from "react";
 import "react-quill/dist/quill.snow.css";
-import { MainContainer } from "@/styles/workspace/Script.styles";
+import { EditorWrapper, MainContainer } from "@/styles/workspace/Script.styles";
 import CustomToolbar from "@/components/workspace/script/Toolbar";
 import QuillEditor from "@/components/workspace/script/Editor";
 
-export default function Script({
-  params: { workspace_id },
-}: {
-  params: { workspace_id: string };
-}) {
-  console.log(workspace_id);
+export default function Script() {
   const editorRef = useRef(null);
   const mainRef = useRef(null);
 
   return (
-    <>
+    <EditorWrapper>
       <CustomToolbar editorRef={editorRef} />
       <MainContainer ref={mainRef}>
         <QuillEditor innerRef={editorRef} MainRef={mainRef} />
       </MainContainer>
-    </>
+    </EditorWrapper>
   );
 }
