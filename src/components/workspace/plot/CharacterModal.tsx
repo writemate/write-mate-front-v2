ece4d0d9c21b6eb7dcc27225358bdbb09840e98a
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2024-10-16 21:37:16 +0900

    [design] 버튼 이쁘게~

--- src/components/workspace/plot/CharacterModal.tsx
+++ src/components/workspace/plot/CharacterModal.tsx
@@ -4,6 +4,7 @@ import { TPlotEventCharacter } from "@/utils/APIs/types";
 import { useState } from "react";
 import styled from "styled-components";
 import Circulation from "@/assets/workspace/characterModal/circulation.svg";
+import Check from "@/assets/workspace/characterModal/check.svg";
 
 interface CharacterModalProps {
   onClose: () => void;
@@ -22,19 +23,35 @@ export default function CharacterModal({
   // 자동연동
   const handleGetChatacter = () => {};
 
-  //토스트알림
+  // 토스트알림
 
-  //캐릭터 선택
+  // 캐릭터 선택
+  const handleAddCharacter = () => {};
+
+  // 캐릭터 선택 취소
+  const handleDeleteCharacter = () => {};
 
   return (
     <ModalContainer ref={ref}>
-      <AutoBtn onClick={handleGetChatacter}>
-        <Circulation />
-        자동 연동
-      </AutoBtn>
+      <div>
+        <AutoBtn onClick={handleGetChatacter}>
+          <Circulation style={{ marginRight: "6px" }} />
+          자동 연동
+        </AutoBtn>
+        {characters.map((character) => (
+          <CharacterCheckBtn
+            onClick={handleDeleteCharacter}
+            key={character._id}
+          >
+            <Check style={{ marginRight: "6px" }} />
+            {character.ch_name}
+          </CharacterCheckBtn>
+        ))}
+      </div>
+
       <div>
         {characters.map((character) => (
-          <CharacterDefaultBtn key={character._id}>
+          <CharacterDefaultBtn onClick={handleAddCharacter} key={character._id}>
             {character.ch_name}
           </CharacterDefaultBtn>
         ))}
@@ -44,12 +61,9 @@ export default function CharacterModal({
 }
 
 const AutoBtn = styled.button`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
+  display: inline-block;
   padding: 12px 20px 12px 16px;
-  gap: 10px;
+  margin: 0 18px 20px 0;
 
   height: 44px;
   border: none;
@@ -61,15 +75,13 @@ const AutoBtn = styled.button`
 
   font-weight: 700;
   font-size: 16px;
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
+  cursor: pointer;
 `;
 
 const CharacterDefaultBtn = styled.button`
   box-sizing: border-box;
   margin: 0 8px 8px 0;
+  cursor: pointer;
 
   display: inline-block;
   padding: 12px 20px;
@@ -83,6 +95,11 @@ const CharacterDefaultBtn = styled.button`
   background: rgba(255, 255, 255, 0.002);
   border: 1px solid ${colorSystem.gray300};
   border-radius: 100px;
+`;
+
+const CharacterCheckBtn = styled(CharacterDefaultBtn)`
+  color: ${colorSystem.orange400};
+  border-color: ${colorSystem.orange400};
 `;
 
 const ModalContainer = styled.div`
@@ -102,4 +119,3 @@ const ModalContainer = styled.div`
   box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.25);
   border-radius: 8px;
 `;
-//calc(50% - 168px / 2 - 108px);
