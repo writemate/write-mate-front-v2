1e92f9f75134e87f5684499a7ae4d0e384edcee0
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2024-10-24 18:35:45 +0900

    [feat] 인물 선택 삭제 mock

--- src/components/workspace/plot/CharacterModal.tsx
+++ src/components/workspace/plot/CharacterModal.tsx
@@ -11,7 +11,11 @@ import {
   SelectBtnWrapper,
 } from "@/styles/workspace/plot/CharacterModal.styles";
 import showToastMessage from "@/hooks/workspace/plot/showToastMessage";
-import { mockCharacterList, PlotCharacterType } from "@/utils/APIs/mock/plot";
+import {
+  CharacterListType,
+  mockCharacterList,
+  PlotCharacterType,
+} from "@/utils/APIs/mock/plot";
 
 interface CharacterModalProps {
   onClose: () => void;
@@ -26,30 +30,45 @@ export default function CharacterModal({
     onClose();
   });
 
-  const [selectCharacters, setSelectCharacter] =
+  const [selectCharacters, setSelectCharacters] =
     useState<PlotCharacterType[]>(character);
-
-  // 자동연동
-  const handleAutoChatacter = () => {};
 
   // 전체 캐릭터 리스트 가져오기
   //const { characterList } = useCharacterList();
   const characterList = mockCharacterList;
 
-  const remainingCharacters = characterList.filter(
-    (character) =>
-      !selectCharacters.some((selected) => selected.id === character.id)
-  );
+  const [remainingCharacters, setRemainingCharacters] =
+    useState<CharacterListType[]>(characterList);
+
+  useEffect(() => {
+    // selectCharacters가 변경될 때마다 remainingCharacters를 업데이트
+    setRemainingCharacters(
+      characterList.filter(
+        (character) =>
+          !selectCharacters.some((selected) => selected.id === character.id)
+      )
+    );
+  }, [selectCharacters, characterList]);
+
+  // 자동연동
+  const handleAutoChatacter = () => {};
 
   // 캐릭터 선택
-  const handleAddCharacter = () => {
-    console.log("select");
+  const handleAddCharacter = (character: CharacterListType) => {
+    // 타입 다르니까 맞춰서 넣어주기..
+    const newCharacter: PlotCharacterType = {
+      id: character.id,
+      ch_image: character.ch_image,
+      ch_name: character.ch_name,
+    };
+    setSelectCharacters((prev) => [...prev, newCharacter]);
+
     showToastMessage("인물이 추가되었습니다.", "success");
   };
 
   // 캐릭터 선택 취소
-  const handleDeleteCharacter = () => {
-    console.log("delete");
+  const handleDeleteCharacter = (character: PlotCharacterType) => {
+    setSelectCharacters((prev) => prev.filter((ch) => ch.id !== character.id));
     showToastMessage("인물이 삭제되었습니다", "success");
   };
 
@@ -63,7 +82,7 @@ export default function CharacterModal({
         {selectCharacters.map((selectCharacter) => (
           <CharacterCheckBtn
             type="button"
-            onClick={handleDeleteCharacter}
+            onClick={() => handleDeleteCharacter(selectCharacter)}
             key={selectCharacter.id}
           >
             <Check style={{ marginRight: "6px" }} />
@@ -75,7 +94,7 @@ export default function CharacterModal({
         {remainingCharacters?.map((character) => (
           <CharacterDefaultBtn
             type="button"
-            onClick={handleAddCharacter}
+            onClick={() => handleAddCharacter(character)}
             key={character.id}
           >
             {character.ch_name}
