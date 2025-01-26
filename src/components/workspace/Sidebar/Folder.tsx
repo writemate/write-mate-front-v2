47681cddcc1e93ccdf921e0d7d08ac54f7506162
raipen <raipendalk@gmail.com>
2025-01-26 21:16:23 +0900

    refactor: linting Folder.tsx

--- src/components/workspace/Sidebar/Folder.tsx
+++ src/components/workspace/Sidebar/Folder.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { useContext } from "react";
 import { useKebab } from "@/hooks/workspace/sidebar/useKebab";
 import { TFolderWithOptions } from "@/utils/APIs/types";
@@ -7,49 +7,110 @@ import CloseDropButton from "@/assets/workspace/sideBar/closedDropButton.svg";
 import FolderIcon from "@/assets/workspace/sideBar/folder.svg";
 import SeletedFolder from "@/assets/workspace/sideBar/selectedFolder.svg";
 import { SidebarContext } from "@/stores/sidebarContext";
-import { FileListContainer, FolderContainer, FolderFileContainer, FolderName,
-  KebabWrapper, Kebab, KebabContainer, KebabItem, TopDropLine } from "@/styles/workspace/SideBar.styles";
+import {
+  FileListContainer,
+  FolderContainer,
+  FolderFileContainer,
+  FolderName,
+  KebabWrapper,
+  Kebab,
+  KebabContainer,
+  KebabItem,
+  TopDropLine,
+} from "@/styles/workspace/SideBar.styles";
 import { useDrag } from "@/hooks/workspace/sidebar/useDrag";
 import File from "@/components/workspace/Sidebar/File";
 
-export default function Folder({ folder, nestedLevel = 0, type}:
-  { folder: TFolderWithOptions, nestedLevel?: number, type: "plot" | "script" }) {
-
-  const { toggleFolder, openFolder, onBlur, onKeyDown, changeName, deleteFolderOrFile } = useContext(SidebarContext);
+export default function Folder({
+  folder,
+  nestedLevel = 0,
+  type,
+}: {
+  folder: TFolderWithOptions;
+  nestedLevel?: number;
+  type: "plot" | "script";
+}) {
+  const {
+    toggleFolder,
+    openFolder,
+    onBlur,
+    onKeyDown,
+    changeName,
+    deleteFolderOrFile,
+  } = useContext(SidebarContext);
   const { isKebabOpen, openKebab, closeKebab } = useKebab();
-  const { isDragOverAfter, isDragOverBefore, onDragStart, onDragOver, onDragLeave, onDrop} = useDrag(folder);
-  
+  const {
+    isDragOverAfter,
+    isDragOverBefore,
+    onDragStart,
+    onDragOver,
+    onDragLeave,
+    onDrop,
+  } = useDrag(folder);
+
   return (
     <FolderContainer>
       <FolderFileContainer
-        onClick={openFolder(folder)} $nestedLevel={nestedLevel} $isSelect={folder.isSelect} $dragOver={isDragOverAfter} $isEditing={folder.isEditing}
-        onDragStart={onDragStart} onDragOver={onDragOver} onDragLeave={onDragLeave} onDrop={onDrop} draggable={true}
+        onClick={openFolder(folder)}
+        $nestedLevel={nestedLevel}
+        $isSelect={folder.isSelect}
+        $dragOver={isDragOverAfter}
+        $isEditing={folder.isEditing}
+        onDragStart={onDragStart}
+        onDragOver={onDragOver}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}
+        draggable={true}
       >
         <TopDropLine $nestedLevel={nestedLevel} $active={isDragOverBefore} />
         {folder.isOpen && <OpendDropButton onClick={toggleFolder(folder)} />}
         {!folder.isOpen && <CloseDropButton onClick={toggleFolder(folder)} />}
         {folder.isSelect && <SeletedFolder />}
         {!folder.isSelect && <FolderIcon />}
-        {!folder.isEditing&&<FolderName>{folder.folder_name}</FolderName>}
-        {folder.isEditing&&
-          <input type="text" defaultValue={folder.folder_name} onBlur={onBlur(folder)} onKeyDown={onKeyDown(folder)}
-            autoFocus/>
-        }
+        {!folder.isEditing && <FolderName>{folder.folder_name}</FolderName>}
+        {folder.isEditing && (
+          <input
+            type="text"
+            defaultValue={folder.folder_name}
+            onBlur={onBlur(folder)}
+            onKeyDown={onKeyDown(folder)}
+            autoFocus
+          />
+        )}
         <KebabWrapper tabIndex={0} onBlur={closeKebab}>
           <Kebab onClick={openKebab} />
-          {isKebabOpen && <KebabContainer onClick={closeKebab}>
-            <KebabItem onClick={changeName(folder)}>이름 변경</KebabItem>
-            <KebabItem onClick={deleteFolderOrFile(folder)}>삭제하기</KebabItem>
-          </KebabContainer>}
+          {isKebabOpen && (
+            <KebabContainer onClick={closeKebab}>
+              <KebabItem onClick={changeName(folder)}>이름 변경</KebabItem>
+              <KebabItem onClick={deleteFolderOrFile(folder)}>
+                삭제하기
+              </KebabItem>
+            </KebabContainer>
+          )}
         </KebabWrapper>
       </FolderFileContainer>
       <FileListContainer>
-        {folder.isOpen&&folder.files.map((subFile, i) => {
-          if (subFile.isFolder) {
-            return <Folder key={i + 1} folder={subFile} nestedLevel={nestedLevel + 1} type={type} />;
-          }
-          return <File key={i + 1} file={subFile} nestedLevel={nestedLevel + 1} type={type} />;
-        })}
+        {folder.isOpen &&
+          folder.files.map((subFile, i) => {
+            if (subFile.isFolder) {
+              return (
+                <Folder
+                  key={i + 1}
+                  folder={subFile}
+                  nestedLevel={nestedLevel + 1}
+                  type={type}
+                />
+              );
+            }
+            return (
+              <File
+                key={i + 1}
+                file={subFile}
+                nestedLevel={nestedLevel + 1}
+                type={type}
+              />
+            );
+          })}
       </FileListContainer>
     </FolderContainer>
   );
