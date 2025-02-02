0207ec20c1a64b6b014ebeb971dfec7858a6cf02
Suin Lee <xhfntndls82@knu.ac.kr>
2025-02-02 18:05:34 +0900

    feat: add link to create new character in SelectCharacterModal

--- src/components/workspace/plot/SelectCharacterModal.tsx
+++ src/components/workspace/plot/SelectCharacterModal.tsx
@@ -5,6 +5,7 @@ import {
   CharacterCheckBtn,
   CharacterDefaultBtn,
   ModalContainer,
+  MoveLink,
   SelectBtnWrapper,
 } from "@/styles/workspace/plot/CharacterModal.styles";
 import useSelectCharacterModal from "@/hooks/workspace/plot/useSelectCharacterModal";
@@ -16,6 +17,7 @@ import {
 } from "@/hooks/workspace/character/characterList";
 import { getCharacter } from "@/utils/APIs/workspace/character";
 import { getName } from "@/utils/getCharacterName";
+import { MoveToCharacter } from "../info/MainCharacter";
 
 interface CharacterModalProps {
   chapterId: string;
@@ -30,7 +32,7 @@ export default function SelectCharacterModal({
   modalRef,
   selectedCharacterList,
 }: CharacterModalProps) {
-  const { characterList } = useCharacterList();
+  const { characterList, workspace_id } = useCharacterList();
   const {
     onSelectCharacterClick,
     onUnselectCharacterClick,
@@ -52,7 +54,9 @@ export default function SelectCharacterModal({
           <Circulation style={{ marginRight: "6px" }} />
           자동 연동
         </AutoBtn> */}
-
+        <MoveLink href={`/${workspace_id}/character`}>
+          인물 생성하러 가기
+        </MoveLink>
         {selectedCharacterList.map((selectCharacter) => (
           <CharacterCheckBtn
             type="button"
@@ -60,7 +64,7 @@ export default function SelectCharacterModal({
             key={selectCharacter.id}
           >
             <Check style={{ marginRight: "6px" }} />
-            {selectCharacter.ch_name}
+            {getName(selectCharacter)}
           </CharacterCheckBtn>
         ))}
       </SelectBtnWrapper>
